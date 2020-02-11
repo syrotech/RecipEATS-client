ec9aad2c69fbb8797b89dc86f59dba501603aff8
jaemcu <cupp.j.e@gmail.com>
2020-02-10 22:59:10 -0500

    Feature: Add Styling
    1. Added pictures to index page
    2. Added divider line on top

--- src/components/routes/Recipes.js
+++ src/components/routes/Recipes.js
@@ -38,18 +38,45 @@ const Recipes = props => {
       })
   }, [])
 
+  const styleImg = {
+    width: 'inherit',
+    height: '80%'
+  }
+
+  const styleCard = {
+    border: '1px solid rgba(255,255,255,0)',
+    'border-radius': '5%',
+    margin: '0 10px',
+    'margin-top': '10px',
+    padding: '0px',
+    'box-shadow': '0 0 5px grey',
+    overflow: 'hidden'
+  }
+
+  const styleLink = {
+    'margin-top': 'auto',
+    'font-weight': 'bold',
+    'font-size': '1em',
+    display: 'block'
+  }
+
   const recipesJsx = recipes.map(recipe => (
-    <li key={recipe.id}>
-      <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
-    </li>
+
+    <div className="col-lg-2 col-md-3 col-sm-4 col-xs-6 text-center" key={recipe.id} style={styleCard}>
+      <img className="image-in-card" src={recipe.imageurl} style={styleImg}/>
+      <Link style={styleLink} to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
+    </div>
+
   ))
 
   return (
     <Layout>
       <h4>Recipes</h4>
-      <ul>
-        {recipesJsx}
-      </ul>
+      <div className="container">
+        <div className="row">
+          {recipesJsx}
+        </div>
+      </div>
     </Layout>
   )
 }
--- src/components/shared/Nav.js
+++ src/components/shared/Nav.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
+const linkStyle = {
+  display: 'inline-block',
+  width: '32%',
+  'text-align': 'center',
+  'margin-top': '1rem',
+  'font-weight': 'bold'
+}
+
+const hrStyle = {
+  border: '0',
+  height: '1px',
+  'background-image': 'linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0,0, 0.75), rgba(0, 0, 0, 0))'
+}
+
 const Nav = () => (
   <nav>
-    <NavLink to='/'>Home</NavLink>
-    <NavLink to='/recipes'>Recipes</NavLink>
-    <NavLink to='/create-recipe'>Create Recipe</NavLink>
+    <NavLink style={linkStyle} to='/'>Home</NavLink>
+    <NavLink style={linkStyle} to='/recipes'>Recipes</NavLink>
+    <NavLink style={linkStyle} to='/create-recipe'>Create Recipe</NavLink>
+    <hr style={hrStyle} />
   </nav>
 )
 
